2786b5efa8beba4d523c5788b18f9e51b2519ea3
marcoiligan <miligan17@gmail.com>
2023-03-27 11:37:42 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react'
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar"
 import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
       <About />
       <Portfolio />
       <Experience />
+      <Contact />
 
       <SocialLinks />
     </div>
