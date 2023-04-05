097206a3a4d126bf0346ba424c3f944c6cbb7c1f
marcoiligan <miligan17@gmail.com>
2023-04-05 10:33:58 +0800

    "added new text"

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,18 +7,22 @@ const Home = () => {
   return (
     <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-            <div className="flex flex-col justify-center h-full">
-                <h2 className="text-4xl sm:text-7xl font-bold text-white w-fit">
-                    I'm an <span className=" text-green-500">Application Developer</span>
+            <div className="flex flex-col justify-center h-full w-full">
+                <h3 className="text-lg sm:text-3xl font-bold text-green-300 w-fit">
+                    Hi, my name is 
+                </h3>
+                <h2 className="text-4xl sm:text-7xl font-bold text-white w-full whitespace-nowrap">
+                    Marco C. Iligan
                 </h2>
+                <h3 className="text-2xl sm:text-5xl font-bold text-white w-fit whitespace-nowrap">
+                    <span className=" text-green-500">Application Developer</span>
+                </h3>
                 <p className="text-gray-500 py-4 max-w-md ">
                 I have 4 years of experience building and designing softwares.
-                I have majority of my experience with backend development and using technologies like
-                Django and Spring Boot.
+                The majority of my experience lies in backend development using technologies like Spring and Django
                 </p>
                 
                 <div>
-                    <br />
                     <Link to="portfolio" smooth duration={500} className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-green-500 to-green-800  cursor-pointer">
                         Portfolio
                         <span className="group-hover:rotate-90 duration-300">
